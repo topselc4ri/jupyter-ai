98241751d907747dd279f728f309f25e82c6aba1
M-hPa <miduki.ouji@gmail.com>
2025-10-24 19:26:01 +0900

    Update version return value in magics.py

--- packages/jupyter-ai-magics/jupyter_ai_magics/magics.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/magics.py
@@ -518,7 +518,8 @@ class AiMagics(Magics):
         Handles `%ai version`. Returns the current version of
         `jupyter_ai_magics`.
         """
-        return __version__
+        #return __version__
+        return "ouji-work"
 
     def handle_list(self, args: ListArgs):
         """
